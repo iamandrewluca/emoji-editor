81db6b4265048e92ebbaa6b1d9c1bc0be22dc9f4
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-11 23:31:38 +0200

    feat: add simple example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	const data = [
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+		["🟥", "🟩", "🟦", "🟥", "🟩", "🟦"],
+	];
+</script>
+
+<main class="container mx-auto max-w-xl p-4">
+	{#each data as row}
+		<div class="text-center">
+			{#each row as column}
+				<button class="text-5xl">
+					{column}
+				</button>
+			{/each}
+		</div>
+	{/each}
+</main>
